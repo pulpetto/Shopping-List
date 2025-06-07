01aee9a1fd2c0e985754f22cbec6bfd01102a692
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-07 07:05:30 +0200

    convert to stateful widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
-  void _addItem() {}
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
